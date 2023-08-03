9f9d489fbc21b95f5397960865a172c36b5fb40a
Stephen Oluwatunmise <12stephenbrowne@gmail.com>
2023-08-03 11:34:18 +0100

    Added Getters and Setters

--- java-api/src/main/java/com/db/grad/javaapi/service/Security.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/Security.java
@@ -12,4 +12,84 @@ public class Security {
     private float face_value;
     private String currency;
     private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsin() {
+        return isin;
+    }
+
+    public void setIsin(String isin) {
+        this.isin = isin;
+    }
+
+    public String getCusip() {
+        return cusip;
+    }
+
+    public void setCusip(String cusip) {
+        this.cusip = cusip;
+    }
+
+    public String getIssuer_name() {
+        return issuer_name;
+    }
+
+    public void setIssuer_name(String issuer_name) {
+        this.issuer_name = issuer_name;
+    }
+
+    public String getMaturity_date() {
+        return maturity_date;
+    }
+
+    public void setMaturity_date(String maturity_date) {
+        this.maturity_date = maturity_date;
+    }
+
+    public float getCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(float coupon) {
+        this.coupon = coupon;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getFace_value() {
+        return face_value;
+    }
+
+    public void setFace_value(float face_value) {
+        this.face_value = face_value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
