c74f181c1c6a1d1ba17b16f598161df3e6c2f175
Tanvi Pandey <tannypandey@gmail.com>
2023-08-03 10:48:52 +0100

    classes done

---
+++ java-api/src/main/java/com/db/grad/javaapi/service/Security.java
@@ -0,0 +1,15 @@
+package com.db.grad.javaapi.service;
+
+public class Security {
+    private int id;
+    private String isin;
+
+    private String cusip;
+    private String issuer_name;
+    private String maturity_date;
+    private float coupon;
+    private String type;
+    private float face_value;
+    private String currency;
+    private String status;
+}
