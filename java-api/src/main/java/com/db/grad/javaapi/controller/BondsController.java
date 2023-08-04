af4bc5e1ff139f07265013cf93ef3f0162ef34d1
Tanvi Pandey <tannypandey@gmail.com>
2023-08-04 13:43:37 +0100

    fronted working

--- java-api/src/main/java/com/db/grad/javaapi/controller/BondsController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BondsController.java
@@ -5,10 +5,12 @@ import com.db.grad.javaapi.service.IBondsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class BondsController {
 
     @Autowired
