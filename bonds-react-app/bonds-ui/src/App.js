ac12bbb76ddf24e5728401226df43060caf99294
dariuspetriceanu <darius.petriceanu@stud.acs.upb.ro>
2023-08-04 10:56:26 +0100

    Darius - React QL1-3

---
+++ bonds-react-app/bonds-ui/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import BondDetail from './components/BondDetail';
+import AllBonds from './components/AllBonds';
+
+function App() {
+  return (
+    <AllBonds/>
+  );
+}
+
+export default App;
